b04a8fe13b5400989b07e56eb646d78536d42690
factrc <sergey.loskutov@gmail.com>
2022-07-18 15:09:04 +1100

    cosmetic format

--- pammount.sh
+++ pammount.sh
@@ -13,6 +13,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>. 
 
+
 if [ -n "$(readlink -fq $0)" ]; then
  . $(dirname $(readlink -fq $0))/function
 else
